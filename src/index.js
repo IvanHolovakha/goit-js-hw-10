07054c19c174b483e8a05de2c5a58c2b32dca247
IvanHolovakha <meisuiwanreiser@gmail.com>
2022-12-21 22:46:01 +0200

    update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './css/styles.css';
+import debounce from "lodash.debounce";
 
 const DEBOUNCE_DELAY = 300;
+
+const textInput = document.querySelector("#search-box");
+
+textInput.addEventListener("input", debounce(onInput, DEBOUNCE_DELAY))
+
+function onInput (event){
+    const countryName = event.target.value;
+    fetchCountries(countryName)
+}
+
+function fetchCountries(name){
+    fetch(`https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags,languages`)
+    .then(response => {
+        if (!response.ok) {
+        throw new Error(response.status);
+        }
+      return response.json();
+    })
+    .then(data => console.log(data))
+    .catch(error => {
+        console.log(error)
+      });
+}
